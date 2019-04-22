2a10b6dfb63066c0cff19da5e7b8238a1cfaea58
brellin <cekakid@gmail.com>
2019-04-22 16:42:33 -0500

    put now working

--- index.js
+++ index.js
@@ -13,10 +13,36 @@ server.get('/api/users', (req, res) => {
             res.status(201).json(users)
         })
         .catch(err => {
-            res.status(500).json({
-                error: err,
-                message: "That didn't work. Try again."
-            })
+            res
+                .status(500)
+                .json({
+                    error: "The user's information could not be retrieved."
+                })
+        })
+})
+
+server.get(`/api/users/:id`, (req, res) => {
+    const { id } = req.params
+    db
+        .findById(id)
+        .then(user => {
+            user ?
+                res
+                    .status(201)
+                    .json(user) :
+                res
+                    .status(404)
+                    .json({
+                        message: "The user with the specified ID does not exist."
+                    })
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({
+                    error: err,
+                    message: 'Something went wrong. Try again.'
+                })
         })
 })
 
@@ -25,34 +51,85 @@ server.post('/api/users', (req, res) => {
     db
         .insert(newUser)
         .then(user => {
-            res
-                .status(201)
-                .json({
-                    user: newUser.name,
-                    id: user.id
-                })
+            user ?
+                res
+                    .status(201)
+                    .json({
+                        user: newUser.name,
+                        bio: newUser.bio,
+                        id: user.id
+                    }) :
+                res
+                    .status(400)
+                    .json({
+                        error: 'Please provide name AND bio for the user'
+                    })
         })
         .catch(err => {
-            res.json({
-                error: err,
-                message: "Something went wrong. Try again."
-            })
+            res
+                .status(500)
+                .json({
+                    error: err,
+                    message: "There was an error while saving the user to the database."
+                })
         })
 })
 
 server.delete('/api/users/:id', (req, res) => {
-    const userId = req.params.id
+    const { id } = req.params
     db
-        .remove(userId)
+        .remove(id)
         .then(user => {
-            res.status(204).end()
+            user ?
+                res.end() :
+                res
+                    .status(404)
+                    .json({
+                        message: "The user with the specified ID does not exist."
+                    })
         })
         .catch(err => {
-            res.json({
-                error: err,
-                message: 'Something went wrong. Try again.'
-            })
+            res
+                .status(500)
+                .json({
+                    error: "The user could not be removed"
+                })
         })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    if (req.body.name && req.body.bio) {
+        db
+            .update(id, req.body)
+            .then(user => {
+                user ?
+                    res
+                        .status(200)
+                        .json({
+                            name: req.body.name,
+                            bio: req.body.bio
+                        }) :
+                    res
+                        .status(404)
+                        .json({
+                            message: "The user with the specified ID does not exist."
+                        })
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({
+                        error: "The user information could not be modified."
+                    })
+            })
+    } else {
+        res
+            .status(400)
+            .json({
+                errorMessage: "Please provide name AND bio for the user."
+            })
+    }
 })
 
 server.listen(5000)
